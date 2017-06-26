978f9d25ee56e55e974914405c2140d23a196923
nguyenlyminhman <nguyenlyminhman@gmail.com>
2017-06-26 14:15:21 +0700

    add babel core

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 ReactDOM.render(
-    React.createElement('a',{href:'#'},'NLMM'),
+    // React.createElement('a',{href:'#'},'NLMM')
+    <a href='#'  >NLMM </a>,
     document.getElementById('root')
 )
+
