a22767e85805ac33d2d85b26c8a742988a267ee5
nguyenlyminhman <nguyenlyminhman@gmail.com>
2017-06-26 13:50:10 +0700

    add react to app.js

---
+++ app/app.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    React.createElement('a',{href:'#'},'NLMM'),
+    document.getElementById('root')
+)
---
+++ app/comps/hello.js
@@ -0,0 +1,5 @@
+function sayHello() {
+    document.write("Hello JS");
+}
+
+module.exports = sayHello;
